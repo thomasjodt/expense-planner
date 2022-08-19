b0f7277ed6c4baca122916184f6f05166af62af7
thomasjodt <jodt5200@gmail.com>
2022-08-19 12:36:56 -0500

    FEAT Add function to format numbers to money

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,7 @@
 export const ControlPresupuesto = ({ presupuesto }) => {
+  const formatBudget = (cantidad) =>
+    cantidad.toLocaleString('es-PE', { style: 'currency', currency: 'PEN' })
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
       <div>
@@ -6,7 +9,13 @@ export const ControlPresupuesto = ({ presupuesto }) => {
       </div>
       <div className='contenido-presupuesto'>
         <p>
-          <span>Presupuesto:</span> {presupuesto}
+          <span>Presupuesto:</span> {formatBudget(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible:</span> {formatBudget(0)}
+        </p>
+        <p>
+          <span>Gastado:</span> {formatBudget(0)}
         </p>
       </div>
     </div>

75dc89918901f04603e406b6c4f6713531f01a79
thomasjodt <jodt5200@gmail.com>
2022-08-19 15:35:29 -0500

    FEAT Complete the rest of the form

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,6 +19,44 @@ export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
       </div>
       <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
         <legend>Nuevo Gasto</legend>
+        <div className='campo'>
+          <label htmlFor='nombre'>Nombre Gasto</label>
+          <input
+            id='nombre'
+            type='text'
+            placeholder='Añade el nombre del gasto'
+          />
+        </div>
+
+        <div className='campo'>
+          <label htmlFor='cantidad'>Cantidad</label>
+          <input
+            id='cantidad'
+            type='text'
+            placeholder='Añade la cantidad del gasto'
+          />
+        </div>
+
+        <div className='campo'>
+          <label htmlFor='categoria'>Categoría</label>
+          <select
+            id='categoria'
+          >
+            <option value=''>-- Seleccione --</option>
+            <option value='ahorro'>Ahorro</option>
+            <option value='comida'>Comida</option>
+            <option value='casa'>Casa</option>
+            <option value='gastos'>Gastos Varios</option>
+            <option value='ocio'>Ocio</option>
+            <option value='salud'>Salud</option>
+            <option value='suscripciones'>Suscripciones</option>
+          </select>
+        </div>
+
+        <input
+          type='submit'
+          value='Añadir el gasto'
+        />
       </form>
     </div>
   )
